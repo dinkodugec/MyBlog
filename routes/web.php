33835ab130b59df5fd9f223ee1f7699a92553501
Dinko <dinko.dugec@gmail.com>
2022-03-09 00:34:18 +0100

    admin pannel create

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-
-
-
-
 Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
-
+Route::get('/admin', 'AdminsController@index')->name('admin.index');
 /* Route::get('/admin', function () {
     return view('admin.index');
 }); */
