32dbc0c2a4d0fcaf6ec4e4c412fdab389159f472
Dinko <dinko.dugec@gmail.com>
2022-03-11 20:27:12 +0100

    displaying individual post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
-    public function show()
+    public function show(Post $post)   //inject a class, Post class and $post object
     {
-       return view('blog-post');
-
+       return view('blog-post', ['post' => $post]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/admin', 'AdminsController@index')->name('admin.index');
-Route::get('/post', 'PostController@show')->name('post');
+Route::get('/post/{post}', 'PostController@show')->name('post');
 /* Route::get('/admin', function () {
     return view('admin.index');
 }); */
